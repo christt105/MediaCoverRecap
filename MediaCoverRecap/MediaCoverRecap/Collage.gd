9dfaa2cf6baf44279f162c89e1503906fd0dcb7a
Christian Martínez de la Rosa <36189169+christt105@users.noreply.github.com>
2024-08-15 21:35:54 +0200

    sort

--- MediaCoverRecap/MediaCoverRecap/Collage.gd
+++ MediaCoverRecap/MediaCoverRecap/Collage.gd
@@ -26,10 +26,34 @@ func clear() -> void:
 func create_collage(media:Array[Media]) -> void:
 	for m in media:
 		var texture_rect = TextureRect.new()
+		texture_rect.set_meta("media", m)
 		texture_rect.name = m.name
 		texture_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
 		grid_container.add_child(texture_rect)
 		m.cover_updated.connect(_cover_updated.bind(texture_rect))
+
+
+func sort(order:Configuration.Order) -> void:
+	var media:Array[Media]
+	for m in grid_container.get_children():
+		media.push_back(m.get_meta("media"))
+	
+	media.sort_custom(sort_media_ascending if order == Configuration.Order.OLDER else sort_media_descending)
+	
+	for i in range(media.size()):
+		var m = media[i]
+		var tr := grid_container.get_child(i) as TextureRect
+		tr.name = m.name
+		tr.texture = m.cover
+		tr.set_meta("media", m)
+	
+
+
+func sort_media_ascending(a:Media, b:Media) -> bool:
+	return a.completed < b.completed
+
+func sort_media_descending(a:Media, b:Media) -> bool:
+	return a.completed > b.completed
 
 
 func _cover_updated(image:ImageTexture, texture_rect:TextureRect) -> void:
--- MediaCoverRecap/MediaCoverRecap/Configuration.gd
+++ MediaCoverRecap/MediaCoverRecap/Configuration.gd
@@ -1,16 +1,20 @@
+class_name Configuration
 extends Control
 
 
 @export var collage:Collage
 
 @export var columns:SpinBox
-@export var order:OptionButton
+@export var sort:OptionButton
 @export var image_size_info:Label
+
+enum Order {OLDER, NEWER}
 
 
 func _ready() -> void:
 	collage.resized.connect(_on_collage_resized)
 	columns.value_changed.connect(_on_columns_changed)
+	sort.item_selected.connect(_on_sort_changed)
 
 
 func _on_collage_resized(size:Vector2i) -> void:
@@ -19,3 +23,7 @@ func _on_collage_resized(size:Vector2i) -> void:
 
 func _on_columns_changed(value:float) -> void:
 	collage.columns = int(value)
+
+
+func _on_sort_changed(index:int) -> void:
+	collage.sort(index)
